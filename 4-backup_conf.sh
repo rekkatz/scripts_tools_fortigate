5ee22e82f19708259f67bfbe5fc4b830d5b814a3
rekkatz <20032883+rekkatz@users.noreply.github.com>
2020-01-13 09:34:35 +0100

    Update 4-backup_conf.sh

--- 4-backup_conf.sh
+++ 4-backup_conf.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/expect -f
 
 # Variables definidas
-set NAME [lindex $argv 0];
+set PATH [lindex $argv 0];
 set DATE [exec date +%F_%T];
 set CONFIG "sys_config"
 set timeout 2;
 
 # Log file
-log_file <create log file>
+log_file <Log_path>
 
 # Execution command to client Fortigate CLI
-spawn scp -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=~/.ssh/known_hosts" -P 22 user@ip:$CONFIG <pathsaveconf>/$NAME\_$DATE.conf
+spawn scp -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=~/.ssh/known_hosts" -P 22 user@ip:$CONFIG $PATH/backup\_$DATE.conf
 expect "Enter password: "
-send "password\r" #Password
+send "admin\r"
 expect eof
+

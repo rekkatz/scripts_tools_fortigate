9a74965d1e3442806b1af26d879b8543f71b09b5
rekkatz <20032883+rekkatz@users.noreply.github.com>
2020-02-03 12:39:15 +0100

    Update 4-backup_conf.sh

--- 4-backup_conf.sh
+++ 4-backup_conf.sh
@@ -7,10 +7,16 @@ set CONFIG "sys_config"
 set timeout 2;
 
 # Log file
-log_file <PATH_SAVE_LOG>
+log_file &lt;PATH_SAVE_LOG&gt;
+log_user 0
 
 # Execution command to client Fortigate CLI
-spawn scp -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=~/.ssh/known_hosts" -P 22 <USER@IP>:$CONFIG $PATH/backup\_$DATE.conf
+spawn scp -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=~/.ssh/known_hosts" -P 22 &lt;USER@IP&gt;:$CONFIG $PATH/backup\_$DATE.conf
 expect "password: "
-send "password\r"
+send "admin\r"
+puts "#####################################"
+puts "# Realizando Backup a través de SCP #"
+puts "#####################################"
+log_user 1
 expect eof
+puts "\r"
